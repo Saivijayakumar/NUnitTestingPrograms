89a25d00982e7679d77ac57a59c4a984ec526eea
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-02 17:21:17 +0530

    [SaiVijayaKumar] Decimal To Binary Code

---
+++ NUnitTestingProgram/DecimalToBinary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestingPrograms
+{
+    class DecimalToBinary
+    {
+        public static void AnyConvertion()
+        {
+            int n, i;
+            //creating an integer array
+            int[] a = new int[10];
+
+            Console.Write("Enter the number to convert: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            
+            for (i = 0; n > 0; i++)
+            {
+                a[i] = n % 2;
+                n = n / 2;
+            }
+            
+            Console.Write("\nBinary of the given number= ");
+            for (i = i - 1; i >= 0; i--)
+            {
+                Console.Write(a[i]);
+            }
+
+        }
+    }
+}
--- NUnitTestingProgram/Program.cs
+++ NUnitTestingProgram/Program.cs
@@ -16,7 +16,7 @@ namespace NUnitTestingPrograms
             Console.WriteLine("\t\t\tWELCOME\n");
             Console.WriteLine("\tThese are the LIST of some NUnit Testing Programs");
             Console.WriteLine("\t----------------------------------------------");
-            Console.WriteLine("\t 1.Vending Machine\n\t 2.Day Of Week\n\t 3.Temperatur Conversion\n\t 4.Monthly Payment Calculation\n\t 5.Newton's square root\n\t 6.");
+            Console.WriteLine("\t 1.Vending Machine\n\t 2.Day Of Week\n\t 3.Temperatur Conversion\n\t 4.Monthly Payment Calculation\n\t 5.Newton's square root\n\t 6.Decimal To Binary");
             Console.WriteLine("\t----------------------------------------------");
             Console.Write("\tEnter your choice : ");
             switch (Convert.ToInt32(Console.ReadLine()))
@@ -40,6 +40,10 @@ namespace NUnitTestingPrograms
                 case 5:
                     Console.WriteLine("\t Newton's square root Code Starts");
                     Newtonsquare.ReadInput();
+                    break;
+                case 6:
+                    Console.WriteLine("\t Decimal To Binary Code Starts");
+                    DecimalToBinary.AnyConvertion();
                     break;
                 default:
                     Console.WriteLine("\t PLEASE ENTER A VALID OPTION");
