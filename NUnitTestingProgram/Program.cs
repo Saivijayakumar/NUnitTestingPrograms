64b899eb7e5372a4b198e9a813d5513d06714164
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-02 12:05:02 +0530

    [SaiVijayaKumar] Vending Machine Code

--- NUnitTestingProgram/Program.cs
+++ NUnitTestingProgram/Program.cs
@@ -13,7 +13,36 @@ namespace NUnitTestingPrograms
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\t\t\tWELCOME\n");
+            Console.WriteLine("\tThese are the LIST of some NUnit Testing Programs");
+            Console.WriteLine("\t----------------------------------------------");
+            Console.WriteLine("\t 1.Vending Machine\n\t 2.Perfect Number\n\t 3.Prime Number\n\t 4.Reversing Number\n\t 5.");
+            Console.WriteLine("\t----------------------------------------------");
+            Console.Write("\tEnter your choice : ");
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    Console.WriteLine("\t Vending Machine Code Starts");
+                    VendingMachine.GenerateNotes();
+                    break;
+                /*case 2:
+                    Console.WriteLine("\t Perfect Number Code Starts");
+                    PerfectNumber.CheckingPerfectNumber();
+                    break;
+                case 3:
+                    Console.WriteLine("\t Prime Number Code Starts");
+                    PrimeNumber.CheckingPrime();
+                    break;
+                case 4:
+                    Console.WriteLine("\t Reversing Number Code Starts");
+                    ReversingNumber.Reversing();
+                    break;*/
+                default:
+                    Console.WriteLine("\t PLEASE ENTER A VALID OPTION");
+                    break;
+            }
 
+            Console.Read();
         }
     }
 }
---
+++ NUnitTestingProgram/VendingMachine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestingPrograms
+{
+    class VendingMachine
+    {
+        public static void GenerateNotes()
+        {
+            Console.WriteLine("Enter amount to return change");
+            int number = Convert.ToInt32(Console.ReadLine());
+            FindingMinNotes(number);
+        }
+        private static void FindingMinNotes(int amount)
+        {
+            int[] notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            int[] notecount = new int[notes.Length];
+
+            for (int i = 0; i < notes.Length; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    notecount[i] = amount / notes[i];
+                    amount = amount - (notecount[i] * notes[i]);
+                    if (amount <= 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Currency Count");
+            for (int i = 0; i < notes.Length; i++)
+            {
+                if (notecount[i] != 0)
+                {
+                    Console.WriteLine(notes[i] + " : " + notecount[i]);
+                }
+            }
+        }
+    }
+}
